1e804b1905617188e7ef140af55b0d0068ed097c
hungtrn75 <hungtrn75@gmail.com>
2018-03-30 02:47:10 +0700

    Add testing database

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb://localhost:27017/TodoApp';
+const url = process.env.MONGODB_URI;
 
 mongoose.Promise = global.Promise;
 mongoose.connect(url);
