6013a4e91f6b842be9f15b7d6c0cee0bb02f1f7f
hungtrn75 <hungtrn75@gmail.com>
2018-04-03 11:53:32 +0700

    Add POST /users/login

--- server/server.js
+++ server/server.js
@@ -109,6 +109,19 @@ app.post('/users', (req, res) => {
     })
 })
 
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    console.log(body);
+    User.findByCredentials(body.email, body.password).then(user => {
+        return user.generateAuthToken().then(token => {
+            res.header('x-auth', token).send(user);
+        })
+    }).catch(e => {
+        res.status(400).send(e);
+    })
+})
+
+//GET /users/me
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 })
