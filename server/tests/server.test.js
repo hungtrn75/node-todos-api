0e39d6c48d875bfb790ee1a96362cc9f40279193
hungtrn75 <hungtrn75@gmail.com>
2018-04-07 14:57:03 +0700

    Add test for DELETE /users/me/token on logout

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -291,3 +291,22 @@ describe('POST /users/login', (req, res) => {
             })
     })
 })
+
+describe('DELETE /user/me/token', () => {
+    it('should remove auth token on logout', done => {
+        request(app)
+            .delete('/users/me/token')
+            .set('x-auth', users[0].tokens[0].token)
+            .expect(200)
+            .end((err, res) => {
+                if (err) {
+                    res.status(400).send(err);
+                }
+
+                User.findById(users[0]._id).then(user => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch(e => done(e));
+            })
+    })
+})
